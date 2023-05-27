80b788f3d73e10da74695240685165f8c5ca3fa4
itsabubakarshf <hi@abubakarshf.dev>
2023-05-27 15:34:24 +0500

    previous week work/update

--- routes/user.js
+++ routes/user.js
@@ -1,47 +1,39 @@
-// routes/user.js
+const express=require('express');
+const router=express.Router();
+const User=require('../models/User');
 
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
+router.post('/register',async(req,res)=>{
+    const {username,email,password}=req.body;
 
-// Register a new user
-router.post('/register', async (req, res) => {
-
-    const { username, email, password } = req.body;
-
-    // Check if any of the required fields are missing or empty
-    if (!username) {
-        return res.status(400).json({ message: 'Username is missing' });
+    if(!username){
+        return res.status(400).json({message:"Username is required"});
     }
-    if (!email) {
-        return res.status(400).json({ message: 'Email is missing' });
+    if(!email){
+        return res.status(400).json({message:"Email is required"});
     }
-    if (!password) {
-        return res.status(400).json({ message: 'Password is missing' });
+    if(!password){
+        return res.status(400).json({message:"Password is required"});
     }
 
-    // Check if password length is less than 8 characters
-    if (password.length < 8) {
-        return res.status(400).json({ message: 'Password should be at least 8 characters long' });
+    //check password length
+    if(password.length<8){
+        return res.status(400).json({message:"Password must be atleast 8 characters"});
     }
+
     try {
-        // Check if the email is already registered
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Email already registered' });
+        const existingUser=await User.findOne({email:email});
+        if(existingUser){
+            return res.status(400).json({message:"Email already exists"});
+        }else{
+            const user=new User({username,email,password});
+            await user.save()
+            return res.status(201).json({message:"User created successfully"});
         }
-
-        // Create a new user
-        const user = new User({ username, email, password });
-        await user.save();
-
-        res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+        console.error(error)
+        return res.status(500).json({message:"Something went wrong"});
     }
-});
 
+})
 
-
-module.exports = router;
+module.exports=router
